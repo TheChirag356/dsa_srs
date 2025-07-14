1cd8136a26530ff4dafc9d06a7a3cd86c101e468
TheChirag356 <arorachirag12005@gmail.com>
2025-07-14 10:53:38 +0530

    Initialize logging to debug.log in the init function

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"os"
+	"log"
+
 	"github.com/TheChirag356/dsa_srs/ui"
 )
+
+func init() {
+	f, err := os.OpenFile("debug.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(f)
+}
 
 func main() {
 	ui.StartTUI()
